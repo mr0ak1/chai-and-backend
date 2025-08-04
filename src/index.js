a7cde60f1d6ccdd5d165ece24cbf5a82c4d0ed3e
mr0ak1 <abhishekkrtiwary3395@gmail.com>
2025-08-04 12:31:08 +0530

    server

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+
+// import express from "express";
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log("ERRR", error);
+//       throw error;
+
+//       app.listen(process.env.PORT, () => {
+//         `App is listening on ${process.env.PORT}`;
+//       });
+//     });
+//   } catch (error) {
+//     console.error("Error:", error);
+//     throw error;
+//   }
+// })();
